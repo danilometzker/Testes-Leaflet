71af5b87ab8ed705dc787fe0438c199c383f602d
Danilo Metzker <danilometzker@gmail.com>
2025-02-17 16:07:15 -0400

    fix: fix import css

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,5 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import "./index.css";
 import NewApp from "./NewApp.tsx";
 
 createRoot(document.getElementById("root")!).render(
